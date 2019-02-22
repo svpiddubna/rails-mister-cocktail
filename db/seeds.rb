7302f9d921392d16f59a3898ec4c719361636b8d
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-02-22 17:33:19 +0100

    cockatils

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,36 @@ ingredients["drinks"].each do |drink|
   Ingredient.create(name: drink["strIngredient1"])
 end
 
+Cocktail.create!(
+  name: "Gin and Tonic",
+  banner_url: "https://www.shakedrinkrepeat.com/wp-content/uploads/2018/03/the_perfect_gin_and_tonic_recipe_photo.jpg"
+)
+Cocktail.create!(
+  name: "Strawberry Daiquiri",
+  banner_url: "http://www.delscookingtwist.com/wp-content/uploads/2018/03/Strawberry-Daiquiri-with-Cardamom_9988.jpg"
+)
+Cocktail.create!(
+  name: "Mojito",
+  banner_url: "https://cdn.liquor.com/wp-content/uploads/2018/09/04153106/mojito-720x720-recipe.jpg"
+)
+Cocktail.create!(
+  name: "Negroni",
+  banner_url: "https://cdn.liquor.com/wp-content/uploads/2018/05/08110806/negroni-720x720-recipe.jpg"
+)
+Cocktail.create!(
+  name: "Gin and Tonic",
+  banner_url: "https://www.shakedrinkrepeat.com/wp-content/uploads/2018/03/the_perfect_gin_and_tonic_recipe_photo.jpg"
+)
+Cocktail.create!(
+  name: "Strawberry Daiquiri",
+  banner_url: "http://www.delscookingtwist.com/wp-content/uploads/2018/03/Strawberry-Daiquiri-with-Cardamom_9988.jpg"
+)
+Cocktail.create!(
+  name: "Mojito",
+  banner_url: "https://cdn.liquor.com/wp-content/uploads/2018/09/04153106/mojito-720x720-recipe.jpg"
+)
+Cocktail.create!(
+  name: "Negroni",
+  banner_url: "https://cdn.liquor.com/wp-content/uploads/2018/05/08110806/negroni-720x720-recipe.jpg"
+)
+puts "Done!"
